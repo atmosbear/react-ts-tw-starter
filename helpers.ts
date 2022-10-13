de747ac2144eecf72edecece8c05ebf07f93bcb8
atmosbear <mondegreen84@protonmail.com>
2022-10-13 12:54:22 -0700

    Made the tests stronger

--- helpers.ts
+++ helpers.ts
@@ -1,11 +1,17 @@
-export function loopedArray<Type>(originalArray: readonly Type[], startingValue: Type): Type[] | -1 {
-	const shiftIndex: number = originalArray.indexOf(startingValue)
-	if (shiftIndex !== -1) {
-		const shiftedArray: Type[] = []
-		originalArray.forEach((value: Type, index: number) => {
-			shiftedArray.push(originalArray[(index + shiftIndex) % originalArray.length])
-		})
-		return shiftedArray
+
+export function loopArray<Type>(originalArray: Type[], startingValue: Type): Type[] | -1 {
+	if (originalArray !== undefined) {
+		const startAt = originalArray.indexOf(startingValue)
+		if (startAt !== -1) {
+			const loopedArray = [] as Type[]
+			for (const i in originalArray) {
+				loopedArray.push(originalArray[(Number(i) + Number(startAt)) % originalArray.length])
+			}
+			return loopedArray
+		} else {
+			return -1
+		}
+	} else {
+		return -1
 	}
-	return -1
 }
