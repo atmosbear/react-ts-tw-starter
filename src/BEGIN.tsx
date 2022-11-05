dd7a898d070edf5e6fddef0dfed1cfaa9d39852e
atmosbear <mondegreen84@protonmail.com>
2022-11-05 13:42:01 -0700

    Refactored a few things

--- src/BEGIN.tsx
+++ src/BEGIN.tsx
@@ -1,13 +1,10 @@
-/* eslint-disable */ // It's OK because root is never null.
 import {createRoot} from "react-dom/client"
 import React from "react"
 
-
 export default function Start(): JSX.Element {
 	return (
-		<div className="bg-blue-500 text-white text-3xl">Hello!</div>
+		<div className="bg-orange-500 text-white text-3xl">Hello!</div>
 	)
 }
 
-const root = createRoot(document.getElementById("root")!)
-root.render(<Start></Start>)
+createRoot(document.getElementById("root")!).render(<Start />)
